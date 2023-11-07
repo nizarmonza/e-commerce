b0c0fa00772eeedf70dd1ba6e3e982a7fadb3274
nizaralydrus <nizarmonzaa@gmail.com>
2023-11-07 16:11:05 +0700

    second commit

---
+++ src/Context/ShopContext.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { createContext } from "react";
+import all_product from "../Components/Assets/all_product";
+
+export const ShopContext = createContext(null)
+
+const ShopContextProvider = (props)=>{
+    const contextValue = {}
+}
--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
+import NewCollections from '../Components/NewCollections/NewCollections'
+import NewsLetter from '../Components/NewsLetter/NewsLetter'
 import Offers from '../Components/Offers/Offers'
 import Popular from '../Components/Popular/Popular'
 
@@ -9,6 +11,8 @@ const Shop = () => {
       <Hero/>
       <Popular/>
       <Offers/>
+      <NewCollections />
+      <NewsLetter />
     </div>
   )
 }
